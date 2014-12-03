9461cc63fa9d82f850f5a3085486be78ca851afe
Rewasvat <fernando_aluani@hotmail.com>
2014-12-03 16:39:59 -0200

    usando bag of words direito agora

--- visualbags.py
+++ visualbags.py
@@ -1,68 +1,70 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import os
+import os, sys
 import random
 import cv2
 from time import time
 import numpy as np
 
-###
-# 1) Detection and description of image patches
-# 2) Assigning patch descriptors to a set of predetermined clusters (a vocabulary) with a vector quantization algorithm
-# 3) Constructing a Bag Of Keypoints, which counts the number of patches assigned to each cluster
-# 4) Applying a multi-class classifier, treating the bag of keypoints as the feature vector, and thus determine which
-#    category or categories to assign to the image.
-###
-
-###
-#  1? Detection and description of image patches for a set of labeled training images
-#  2? Constructing a set of vocabularies: each is a set of cluster centres, with respect to which descriptors are vector quantized.
-#  3? Extracting bags of keypoints for these vocabularies
-# 4A? Training multi-class classifiers using the bags of keypoints as feature vectors
-# 4B? Selecting the vocabulary and classifier giving the best overall classification accuracy
-###
-
-# Feature Extraction: SIFT
-# Visual Vocabulary Construction: k-means
-# Categorization: naiveBayes ou SVM ou KNN
-
-def t():
-    import os
-    l = os.listdir("../dataset")
-    l.sort()
-    names = []
-    for s in l:
-        n = "_".join( s.split(".")[0].split("_")[:-1] )
-        if not n in names:
-            names.append(n)
-    return l, names
-
-def tt():
-    l,names = t()
-    print len(names)
-    for s in names: print s
-    return l,names
     
 ###################################################
-def extractFeatures(img):
-    sift = cv2.SIFT()
-    keypoints, descriptors = sift.detectAndCompute(img, None)
+def getConsoleWidth():
+    #only works on unix
+    try:
+        rows, columns = os.popen('stty size', 'r').read().split()
+        return int(columns)
+    except:
+        return 80
     
-
-###################################################
-    
-###################################################
+class ProgressBar:
+    def __init__(self, text, totalWork):
+        self.text = text
+        self.totalWork = float(totalWork)
+        self.currentWork = 0
+        self.mark = time()
+        
+    def update(self, add=1):
+        self.currentWork += add
+        sys.stdout.write("\r%s" % self.__str__())
+        sys.stdout.flush()
+        
+    def finish(self):
+        sys.stdout.write("\r%s\n" % self.__str__())
+        sys.stdout.flush()
+        
+    def clear(self):
+        sys.stdout.write("\r" + (" "*(getConsoleWidth())) + "\r")
+        sys.stdout.flush()
+        
+    def __str__(self):
+        s = self.text + ": ["
+        p = self.currentWork/self.totalWork
+        elapsed = time()-mark
+        estimated = elapsed * ( (self.totalWork/self.currentWork) - 1 )
+        timeText = "] (%.2f secs | %.2f secs)" % (elapsed, estimated)
+        barSize = getConsoleWidth() - len(s) - len(timeText)
+        barDone = int(barSize*p)
+        s += "=" * barDone
+        s += " " * (barSize - barDone)
+        s += timeText
+        s += " " * (getConsoleWidth() - len(s))
+        return s
+        
+        
 
 ###################################################
 class DataSet:
-    def __init__(self, path, trainingPercentage, randomTrainingSet, featureExtractor):
+    def __init__(self, path, trainingPercentage, k, randomTrainingSet, featureExtractor):
         self.path = path
         self.featureExtractor = featureExtractor
         self.tp = trainingPercentage
+        self.k = k
         self.rts = randomTrainingSet
         self.classes = {}
         self.classNames = []
+        self.clusters = None
+        self.bag = None
         
     def load(self):
         files = os.listdir(self.path)
@@ -79,31 +81,50 @@ class DataSet:
     def getTrainingData(self):
         values = None
         labels = []
+        trainingDescriptors = None
+
         for c in self.classes.values():
             mark = time()
             # separate training set from class image set
             c.generateTrainingSet(self.tp, self.rts)
-            # generate vocabulary
-            c.calculateClusters(self)
-            
-            if values == None:
-                values = c.clusters
+            # generate descriptors
+            descs = c.getTrainingDescriptors(self)
+            if trainingDescriptors == None:
+                trainingDescriptors = descs
             else:
-                values = np.append(values, c.clusters, 0)
-            labels += [c.label]*c.clusters.shape[0]
+                trainingDescriptors = np.append(trainingDescriptors, descs, 0)
             stepTime = time()-mark
-            print "Calculated clusters for %s (%.2f secs)" % (c.name, stepTime)
+            print "Calculated descriptors for %s (%.2f secs)" % (c.name, stepTime)
+            
+        # Clusterize (generate vocabulary)
+        mark = time()
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+        compactness, bestLabels, self.clusters = cv2.kmeans(trainingDescriptors, self.k, criteria, 10, cv2.KMEANS_PP_CENTERS)   #k = 1000
+        stepTime = time()-mark
+        print "Generated clusters (vocabulary) in %.2f secs" % (stepTime)
+        
+        mark = time()
+        self.bag = cv2.KNearest()
+        self.bag.train(self.clusters, np.arange(self.k))
+        stepTime = time()-mark
+        print "Created bag of visual words from clusters in %.2f secs" % (stepTime)
+        
+        for c in self.classes.values():
+            hists = c.getTrainingHistograms(self)
+            if values == None:
+                values = hists
+            else:
+                values = np.append(values, hists, 0)
+            labels += [c.label]*hists.shape[0]
         return values, np.asarray( labels, dtype='float32')
             
-
-        
+###################################################  
 class DataClass:
     def __init__(self, name, label):
         self.name = name
         self.label = label
         self.indexes = []
         self.trainIndexes = []
-        self.clusters = None
         
     def size(self):
         return len(self.indexes)
@@ -121,19 +142,6 @@ class DataClass:
         sample = random.choice(self.indexes)
         self.indexes.remove(sample)
         return sample
-        
-    def calculateClusters(self, dataset):
-        classDescs = None
-        # Get the descriptors for each image in training set
-        for index in self.trainIndexes:
-            descs = self.getImageDescriptor(index, dataset)
-            if classDescs == None:
-                classDescs = descs
-            else:
-                classDescs = np.append(classDescs, descs, 0)
-        # Clusterize (generate vocabulary)
-        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-        compactness, bestLabels, self.clusters = cv2.kmeans(classDescs, 1000, criteria, 10, cv2.KMEANS_PP_CENTERS)   #k = 1000
 
     def getImageDescriptor(self, index, dataset):
         fileName = dataset.path + self.name + "_" + str(index) + ".jpg"  # hardcoded extension
@@ -141,7 +149,35 @@ class DataClass:
         keypoints, descriptors = dataset.featureExtractor.detectAndCompute(img, None)
         return descriptors
         
+    def getTrainingDescriptors(self, dataset):
+        classDescs = None
+        # Get the descriptors for each image in training set
+        for index in self.trainIndexes:
+            descs = self.getImageDescriptor(index, dataset)
+            classDescs = descs if classDescs==None else np.append(classDescs, descs, 0)
+        return classDescs
+        
+    def getImageHistogram(self, index, dataset):
+        descs = self.getImageDescriptor(index, dataset)
+        response, results, neighborResponsers, dists = dataset.bag.find_nearest(descs, 5 )
+        hist = np.zeros( (1, dataset.k), dtype='float32' )
+        for i in results:
+            hist[0][int(i[0])] += 1
+        return hist
+        
+    def getTrainingHistograms(self, dataset):
+        classHists = None
+        # Get the histogram for each image in training set
+        #print self.name, "TRAINING HISTOGRAMS"
+        bar = ProgressBar("Pegando Histogramas de "+self.name, len(self.trainIndexes))
+        for index in self.trainIndexes:
+            hist = self.getImageHistogram(index, dataset)
+            classHists = hist if classHists==None else np.append(classHists, hist, 0)
+            bar.update()
+        bar.finish()
+        return classHists 
 
+###################################################
 class Classifier:
     def __init__(self, algorithm, stateFile):
         self.cla = algorithm
@@ -157,7 +193,7 @@ class Classifier:
                 print "ERROR: Could not save classifier to file..."
         if runTrain:
             values, labels = data.getTrainingData()
-            self.cla.train( values, labels )
+            self._doTrain( values, labels )
         
     def runTests(self, data, testsPerClass):
         results = {}
@@ -166,7 +202,7 @@ class Classifier:
             oks = 0
             for bla in xrange(testsPerClass):
                 sample = c.popRandomSample()
-                value = c.getImageDescriptor( sample, data)
+                value = c.getImageHistogram(sample, data)
 
                 response = self.predict(value)
                 check = response == c.label
@@ -186,7 +222,7 @@ class Classifier:
         print "WARNING: this should be overwritten in subclasses..."
         return self.cla.predict(sample)
             
-                  
+###################################################  
 class KNNClassifier(Classifier):
     def __init__(self, stateFile):
         Classifier.__init__(self, cv2.KNearest(), stateFile)
@@ -194,13 +230,21 @@ class KNNClassifier(Classifier):
     def predict(self, sample):
         response, results, neighborResponsers, dists = self.cla.find_nearest(sample, 5 )
         return response
-        
+    def _doTrain(self, values, labels):
+        self.cla.train(values, labels)
+
+###################################################
 class SVMClassifier(Classifier):
     def __init__(self, stateFile):
         Classifier.__init__(self, cv2.SVM(), stateFile)
         
     def predict(self, sample):
         return self.cla.predict(sample)
+    def _doTrain(self, values, labels):
+        svm_params = dict( kernel_type = cv2.SVM_LINEAR,
+                           svm_type = cv2.SVM_C_SVC,
+                           C=2.67, gamma=5.383 )
+        self.cla.train(values, labels, params=svm_params)
     
 ###################################################
 
@@ -218,6 +262,7 @@ if __name__ == '__main__':
         elif tp > 1.0:  return 1.0
         return tp
     parser.add_argument("--trainingPercentage", "-tp", type=tpType, default=0.1, help="Threshold percentage of samples in each class to use for training. (default 0.1)")
+    parser.add_argument("--K", "-k", type=int, default=1000, help="Number of clusters in bag of visual words vocabulary. (default 1000)")
     parser.add_argument("--testsPerClass", "-tpc", type=int, default=10, help="Number of tests per class to run. (default 10)")
     parser.add_argument("--load", "-l", action="store_true", default=False, help="If we should try to load classifier state from state file. (default no)")
     parser.add_argument("--save", "-s", action="store_true", default=False, help="If we should try to save classifier state to state file. (default no)")
@@ -229,18 +274,19 @@ if __name__ == '__main__':
     extractOptions = ["SIFT", "SURF"]
     parser.add_argument("--extractor", "-e", choices=extractOptions, default=extractOptions[0], help="Which feature extractor to use. (default SIFT)")
     
-    #qual classifier
-
     args = parser.parse_args()
         
     if args.extractor == "SIFT":
         extractor = cv2.SIFT()
     elif args.extractor == "SURF":
         extractor = cv2.SURF(400)
-    data = DataSet(args.datapath, args.trainingPercentage, args.randomTrainingSet, extractor)
+    data = DataSet(args.datapath, args.trainingPercentage, args.K, args.randomTrainingSet, extractor)
     
     stateFile = (args.stateFile if args.stateFile != "" else "classifierState") if args.load else ""
-    classifier = KNNClassifier( stateFile )
+    if args.classifier == "KNN":
+        classifier = KNNClassifier( stateFile )
+    elif args.classifier == "SVM":
+        classifier = SVMClassifier( stateFile )
     
     total = 0.0
     mark = time()
